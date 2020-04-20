611a404ffd9408f9e5d49afc03dbdc5b5a6a3b15
abhiagrwl <abhiagrwl.it@gmail.com>
2020-04-20 19:22:49 +0530

    updated problem 1 with uc6

--- day10_problem1/problem1.sh
+++ day10_problem1/problem1.sh
@@ -4,8 +4,23 @@ read -p "enter first number" a
 read -p "enter first number" b
 read -p "enter first number" c
 
-res1=$(($a+$b*$c))
-res2=$(($a*$b+$c))
-res3=$(($c+$a/$b))
-res4=$(($a%$b+$c))
+declare -A res
 
+res[res1]=$(($a+$b*$c))
+res[res2]=$(($a*$b+$c))
+res[res3]=$(($c+$a/$b))
+res[res4]=$(($a%$b+$c))
+
+echo ${res[@]}
+
+declare -a array
+counter=0
+
+for i in ${res[@]}
+do
+	echo $res[$i]
+     array[((counter++))]=$res[$i]
+
+done
+
+echo ${array[@]}
